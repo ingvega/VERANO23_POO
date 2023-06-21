6b1c2ffa4087c410372393c4f0d381736d8924cf
ingvega <isc.p.vega@gmail.com>
2023-06-21 17:49:45 -0600

    Add files via upload

--- karel/Prueba.java
+++ karel/Prueba.java
@@ -9,10 +9,32 @@ package unidad2.karel;
  * @author paveg
  */
 public class Prueba {
+
     public static void main(String[] args) {
-        Mundo m=new Mundo(100,100);
-        Robot karel=new Robot("Karel");
-        karel.mundo=m;
+        //No puedo preguntar (llamar un método o atributo de algo null, esdecir,
+        //que aún no es un objeto vivo en memoria)
+//        Mundo m2=null;
+//        System.out.println(m2.getLongitudX());
+
+        Mundo m = new Mundo(5, 5);
+        Robot karel = new Robot("Karel");
+        karel.mundo = m;
+//        System.out.println(karel);
+//        System.out.println(karel.toString());
+//        System.out.println("------------------------------------");
+//        String infoKarel="KAREL " + karel;
+//        String infoMundo="MUNDO " + m;
+//        System.out.println(infoKarel);
+//        System.out.println(infoMundo);
         //karel.camina();
+//        for(int i=1;i<m.getLongitudY();i++){
+//            karel.camina();
+//            System.out.println(karel);
+//        }
+        int contador = 0;
+        while (karel.camina()) {
+            contador++;
+        }
+        System.out.println(contador);
     }
 }
--- karel/Robot.java
+++ karel/Robot.java
@@ -9,54 +9,171 @@ package unidad2.karel;
  * @author paveg
  */
 public class Robot {
-    private String nombre="Anonimo";
-    private char direccion='N';
-    
+
+    private String nombre = "Anonimo";
+    private char direccion = 'N';
+
     public Ubicacion posicionActual;
     public Mundo mundo;
-    
-    public Robot(){
-        posicionActual=new Ubicacion();
+
+    public Robot() {
+        posicionActual = new Ubicacion();
     }
-    
-    public Robot(String nombre){
+
+    public Robot(String nombre) {
         //invocando el contructor predeterminado
         this();
         setNombre(nombre);
         //No se puede invocar directamente el constructor
         //Robot();
-        
+
         //posicionActual=new Ubicacion();
     }
-    
-    
-    private String getNombre(){
+
+    private String getNombre() {
         return nombre;
     }
-    
-    private void setNombre(String nombre){
-        nombre=nombre.trim();
-        if(nombre.length()>=2 && nombre.length()<=20){
-            this.nombre=nombre;
-        }else{
+
+    private void setNombre(String nombre) {
+        nombre = nombre.trim();
+        if (nombre.length() >= 2 && nombre.length() <= 20) {
+            this.nombre = nombre;
+        } else {
             System.out.println("Nombre no válido");
         }
     }
-    
-    public char getDireccion(){
+
+    public char getDireccion() {
         return direccion;
     }
-    
-    public void setDireccion(char direccion){
-        String strDireccion=direccion+"";
+
+    public void setDireccion(char direccion) {
+        String strDireccion = direccion + "";
         //String strDireccion=String.valueOf(direccion);
-        strDireccion=strDireccion.toUpperCase();
-        if(strDireccion.equals("N") || strDireccion.equals("S") ||
-                strDireccion.equals("O") || strDireccion.equals("E")){
-            direccion= strDireccion.charAt(0);
-        }else{
+        strDireccion = strDireccion.toUpperCase();
+        if (strDireccion.equals("N") || strDireccion.equals("S")
+                || strDireccion.equals("O") || strDireccion.equals("E")) {
+            direccion = strDireccion.charAt(0);
+        } else {
             System.out.println("Direccion no válida");
         }
-        
+
+    }
+
+    @Override
+    public String toString() {
+        return "Robot: " + nombre
+                + "\nUbicacion: (" + posicionActual.getX() + ", "
+                + posicionActual.getY() + ")\nDireccion: "
+                + direccion + "\n Mundo: largo " + mundo.getLongitudX()
+                + "    alto " + mundo.getLongitudY();
+    }
+
+    private boolean frenteDespejado() {
+        if (getDireccion() == 'N') {
+            if (posicionActual.getY() < mundo.getLongitudY()) {
+                return true;
+            }
+        } else if (direccion == 'E') {
+            if (posicionActual.getX() < mundo.getLongitudX()) {
+                return true;
+            }
+        } else if (direccion == 'O') {
+            if (posicionActual.getX() > 1) {
+                return true;
+            }
+        } else {
+            if (posicionActual.getY() > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+//    private boolean frenteDespejado() {
+//        switch(direccion){
+//            case 'N':
+//                return posicionActual.getY() < mundo.getLongitudY();
+//            case 'E':
+//                return posicionActual.getX() < mundo.getLongitudX();
+//            case 'O':
+//                return posicionActual.getX() > 1;
+//            default:
+//                return posicionActual.getY() > 1;
+//        }
+//    }
+//    private boolean frenteDespejado() {
+//        if (getDireccion() == 'N') {
+//            if (posicionActual.getY() < mundo.getLongitudY()) {
+//                return true;
+//            } else {
+//                return false;
+//            }
+//        } else if (direccion == 'E') {
+//            if (posicionActual.getX() < mundo.getLongitudX()) {
+//                return true;
+//            } else {
+//                return false;
+//            }
+//        } else if (direccion == 'O') {
+//            if (posicionActual.getX() > 1) {
+//                return true;
+//            } else {
+//                return false;
+//            }
+//        } else {
+//            if (posicionActual.getY() > 1) {
+//                return true;
+//            } else {
+//                return false;
+//            }
+//        }
+//    }
+
+    public void girarDerecha() {
+        if (getDireccion() == 'N') {
+            this.direccion = 'E';
+        } else if (getDireccion() == 'E') {
+            this.direccion = 'S';
+        } else if (getDireccion() == 'S') {
+            this.direccion = 'O';
+        } else if (getDireccion() == 'O') {
+            this.direccion = 'N';
+        }
+    }
+
+    public void girarIzquierda() {
+        if (getDireccion() == 'N') {
+            this.direccion = 'O';
+        } else if (getDireccion() == 'O') {
+            this.direccion = 'S';
+        } else if (getDireccion() == 'S') {
+            this.direccion = 'E';
+        } else if (getDireccion() == 'E') {
+            this.direccion = 'N';
+        }
+    }
+    
+    public boolean camina(){
+        if(frenteDespejado()){
+            if(direccion == 'N'){
+                int NuevaPosicionY;
+                NuevaPosicionY = posicionActual.getY() + 1;
+                posicionActual.setY(NuevaPosicionY);
+                
+            }else if(direccion == 'S'){
+                posicionActual.setY(posicionActual.getY() - 1);
+                
+            }else if(direccion == 'E'){
+                posicionActual.setX(posicionActual.getX() + 1);
+                
+            }else if(direccion == 'O'){
+                 posicionActual.setX(posicionActual.getX() - 1);
+            }
+            
+            return true;
+            
+        }else{
+            return false;
+        }
     }
 }
